406026061ee827d0a222057d2463051d0282b5b6
panchen <panchen@tigerbrokers.com>
2021-07-15 16:56:44 +0800

    Initial commit

---
+++ signature/src/main/java/me/bytebeats/signature/AppSignature.kt
@@ -0,0 +1,10 @@
+package me.bytebeats.signature
+
+/**
+ * Created by bytebeats on 2021/7/15 : 16:50
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+object AppSignature {
+    
+}
